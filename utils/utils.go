d92a99eadc0ccf0f548122cf52a0b3c7eb4819e2
Elly <145195760+ellypaws@users.noreply.github.com>
2024-04-02 08:55:42 +0800

    Write utils to check and retrieve submissions txt/json files

--- utils/utils.go
+++ utils/utils.go
@@ -3,8 +3,12 @@ package utils
 import (
 	"errors"
 	"fmt"
+	"github.com/ellypaws/inkbunny/api"
+	"io"
+	"net/http"
 	"reflect"
 	"strconv"
+	"strings"
 )
 
 // ResultsToFields maps ExtractResult from ExtractAll to fields in a struct.
@@ -62,4 +66,77 @@ func CastStringToType(s string, fieldPtr any) error {
 	}
 
 	return nil
+}
+
+func HasTxtFile(submission api.Submission) bool {
+	for _, file := range submission.Files {
+		if strings.HasPrefix(file.MimeType, "text") {
+			return true
+		}
+	}
+	return false
+}
+
+func HasJsonFile(submission api.Submission) bool {
+	for _, file := range submission.Files {
+		if strings.HasSuffix(file.MimeType, "json") {
+			return true
+		}
+	}
+	return false
+}
+
+func HasMetadata(submission api.Submission) bool {
+	for _, file := range submission.Files {
+		if strings.HasPrefix(file.MimeType, "text") {
+			return true
+		}
+		if strings.HasSuffix(file.MimeType, "json") {
+			return true
+		}
+	}
+	return false
+}
+
+func FilterMetadata(submission api.Submission) (files []api.File) {
+	for _, file := range submission.Files {
+		if strings.HasPrefix(file.MimeType, "text") {
+			files = append(files, file)
+		}
+		if strings.HasSuffix(file.MimeType, "json") {
+			files = append(files, file)
+		}
+	}
+	return files
+}
+
+type MetadataContent struct {
+	Blob []byte
+	api.File
+}
+
+func GetMetadataBytes(submission api.Submission) ([]MetadataContent, error) {
+	var metadata []MetadataContent
+	for _, file := range submission.Files {
+		if !strings.HasPrefix(file.MimeType, "text") && !strings.HasSuffix(file.MimeType, "json") {
+			continue
+		}
+		if file.FileURLFull == "" {
+			continue
+		}
+		r, err := http.Get(file.FileURLFull)
+		if err != nil {
+			return nil, err
+		}
+		defer r.Body.Close()
+		if r.StatusCode != http.StatusOK {
+			return nil, fmt.Errorf("unexpected status code: %v", r.Status)
+		}
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			return nil, err
+		}
+		metadata = append(metadata, MetadataContent{b, file})
+	}
+	return metadata, nil
 }
